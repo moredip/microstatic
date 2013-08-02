dd7c21476861057b65c40bd87fe93d4755cccb99
Pete Hodgson <git@thepete.net>
2013-08-02 00:16:01 -0700

    WIP on a microstatic bin

--- lib/microstatic/s3_bucket_creator.rb
+++ lib/microstatic/s3_bucket_creator.rb
@@ -3,22 +3,15 @@ module Microstatic
 class S3BucketCreator
   include UsesFog
 
-  def initialize( suffix, aws_creds )
+  def initialize( aws_creds )
     check_and_store_aws_creds(aws_creds)
-    if suffix.start_with?(".")
-      @suffix = suffix
-    else
-      @suffix = "."+suffix
-    end
   end
 
-  def create( name )
-    bucket_name = name + @suffix
+  def create( bucket_name )
     connection.put_bucket( bucket_name )
     connection.put_bucket_acl( bucket_name, 'public-read' )
     connection.put_bucket_website( bucket_name, 'index.html', :key => '404.html' )
   end
-
 end
 
 end
