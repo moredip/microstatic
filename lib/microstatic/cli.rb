1804a1ae4e79f807e4d59cdb5f5d85a55c61610c
Pete Hodgson <git@thepete.net>
2013-08-18 00:25:04 -0700

    more WIP on CLI

--- lib/microstatic/cli.rb
+++ lib/microstatic/cli.rb
@@ -3,27 +3,49 @@ require 'rainbow'
 
 module Microstatic
   class CLI < Thor
-    desc "create <BUCKET_NAME>", "create an S3 bucket which can host your static site"
-    def create_bucket( bucket_name = false )
+    desc "setup <APP_NAME>", "do the needful to get started hosting your static site. S3, Route 53, whatever it takes."
+    def setup( app_name = false )
+      app_name ||= inferred_app_name
+      bucket_name = subdomain_for(app_name)
+
+      bucket( bucket_name )
+      dns( app_name )
+    end
+
+    desc "bucket <BUCKET_NAME>", "create an S3 bucket which can host your static site"
+    def bucket( bucket_name = false )
       bucket_name ||= guess_bucket_name
       # TODO: check it doesn't already exist for you
       # TODO: handle the bucket name already being taken by someone else
       # TODO: fail gracefully if aws creds not available
       
-      describe_operation( "create bucket '#{bucket_name}'" ) do
+      describe_operation( "create S3 bucket '#{bucket_name}'" ) do
         S3BucketCreator.new( config.aws_creds ).create( bucket_name )
       end
     end
 
-    desc "deploy <BUCKET_NAME>", "create an S3 bucket which can host your static site"
-    def deploy_to_bucket( bucket_name )
+    desc "dns <APP_NAME>", "create a Route 53 entry pointing to the S3 bucket containing your static site"
+    def dns( app_name = false )
+      app_name ||= inferred_app_name
+      bucket_name = subdomain_for(app_name)
+
+      describe_operation( "create Route 53 entry '#{bucket_name}'" ) do
+        raise 'TODO'
+      end
     end
 
     private 
 
     def guess_bucket_name
-      guessed_app_name = File.basename(Dir.pwd)
-      "#{guessed_app_name}.#{config.site_name}"
+      subdomain_for( inferred_app_name )
+    end
+
+    def subdomain_for( app_name )
+      "#{app_name}.#{config.site_name}"
+    end
+
+    def inferred_app_name
+      File.basename(Dir.pwd)
     end
 
     def config
@@ -31,12 +53,16 @@ module Microstatic
     end
 
     def describe_operation( operation_desc )
-      STDOUT.print "#{operation_desc} ..."
+      STDOUT.print "  #{operation_desc} ..."
       yield
-      STDOUT.puts "\r#{operation_desc}   "+CHECKMARK
+      STDOUT.puts "\r- #{operation_desc}   "+CHECKMARK
+    rescue
+      STDOUT.puts "\r- #{operation_desc}   "+SADMARK
+      raise
     end
 
     CHECKMARK="\u2713".color(:green)
+    SADMARK="\u2718".color(:red)
 
   end
 end
