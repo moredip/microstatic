467b3e81f8e18231fbd350bd58fc833a9bdbbd6a
Max Lincoln <max@devopsy.com>
2013-08-25 20:05:12 -0300

    Ability to use Fog.mock! during tests

--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -1,3 +1,10 @@
 require_relative '../../lib/microstatic'
+# require_relative '../../lib/microstatic/uses_fog'
 
+if ENV['FOG_MOCK']
+  Fog.mock!
+  include Microstatic::UsesFog
+  check_and_store_aws_creds(Microstatic.aws_creds_from_env)
+  connection.directories.create(:key => 'microstatic-test-bucket')
+end
 require 'pry'
